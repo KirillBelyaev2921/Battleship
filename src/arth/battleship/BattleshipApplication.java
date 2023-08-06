829a3a995f44d28c2d48146fdbbb02faecde0144
Kirill Belyaev <kirillbelyaev2921@gmail.com>
2023-08-06 20:10:29 +0300

    Create main menu panel and run simple program with frame.

--- src/arth/battleship/BattleshipApplication.java
+++ src/arth/battleship/BattleshipApplication.java
@@ -1,7 +1,10 @@
 package arth.battleship;
 
+import arth.battleship.controller.BattleshipController;
+
 public class BattleshipApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BattleshipController controller = new BattleshipController();
+        controller.start();
     }
 }
---
+++ src/arth/battleship/gui/MainMenuPanel.java
@@ -0,0 +1,33 @@
+package arth.battleship.gui;
+
+import arth.battleship.controller.MainMenuController;
+
+import javax.swing.*;
+
+public class MainMenuPanel extends JPanel {
+
+    MainMenuController controller;
+    JButton createLobby;
+    JButton joinLobby;
+    JButton exit;
+
+    public MainMenuPanel() {
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        controller = new MainMenuController();
+
+        createLobby = new JButton("Join lobby");
+        createLobby.addActionListener(e -> controller.createLobby());
+
+        joinLobby = new JButton("Join lobby");
+        joinLobby.addActionListener(e -> controller.chooseLobby());
+
+        exit = new JButton("Exit");
+        exit.addActionListener(e -> controller.exit());
+
+        this.add(createLobby);
+        this.add(joinLobby);
+        this.add(exit);
+    }
+
+
+}
